23e61c4180352cab675c9372561d6cec9c72634d
teraitomoya <12a1070.sit.shonan@gmail.com>
2025-07-07 23:38:22 +0900

    4-6_終了

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,17 @@
 import React, { Fragment } from 'react';
 
 export const Restaurants = () => {
+
+  //=====ここから追加する=====
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+//=====ここまで追加する=====
+
+
   return (
     <Fragment>
       レストラン一覧
